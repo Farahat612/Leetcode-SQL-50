f3dbfbc5fd0c0bdbf4875539feabebd6f3b23d50
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-07 01:28:09 +0200

    Section 05 : Advanced Select and Joins - part 01

---
+++ 5. Advanced Select and Joins/02 1789. Primary Department for Each Employee.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT employee_id, department_id
+FROM Employee 
+WHERE employee_id IN
+    (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY 1
+    HAVING COUNT(*) = 1
+    )
+    OR primary_flag = 'Y'
+ORDER BY 1
